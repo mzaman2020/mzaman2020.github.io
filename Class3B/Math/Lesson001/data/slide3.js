9f547728e98e56360cb9ba82824aaadcc864386a
MZ <mzaman2016>
2026-03-18 16:45:20 -0400

    placeholders for new contents and some bengali texts

---
+++ Class3B/Math/Lesson001/data/slide3.js
@@ -0,0 +1,47 @@
+(function(){var loadHandler=window['sl_{ACAB0F35-F42B-41ED-9212-76CA713C4E7F}'];loadHandler&&loadHandler(2, '<div id="spr0_2920723"><div id="spr1_2920723" class="kern slide"><img id="img23_2920723" src="data/img4.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_2920723" class="kern slide"><div id="spr3_2920723" style="left:78.729px;top:33.906px;"><div style="width:0px;"><span id="txt0_2920723" data-width="678.390625" style="left:7.2px;top:-5.53px;">Now it is getting harder to count them</span></div><div style="width:0px;"><span id="txt1_2920723" data-width="586.953125" style="left:7.2px;top:41.99px;">There are just too many of them!</span></div></div><div id="spr4_2920723" style="left:67.297px;top:170.315px;"><img id="img0_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr5_2920723" style="left:109.54px;top:170.315px;"><img id="img1_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr6_2920723" style="left:188.432px;top:170.315px;"><img id="img2_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr7_2920723" style="left:150.73px;top:170.315px;"><img id="img3_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr8_2920723" style="left:225.081px;top:170.315px;"><img id="img4_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr9_2920723" style="left:267.324px;top:170.315px;"><img id="img5_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr10_2920723" style="left:346.216px;top:170.315px;"><img id="img6_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr11_2920723" style="left:308.513px;top:170.315px;"><img id="img7_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr12_2920723" style="left:387.811px;top:170.315px;"><img id="img8_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr13_2920723" style="left:430.054px;top:170.315px;"><img id="img9_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr14_2920723" style="left:508.946px;top:170.315px;"><img id="img10_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr15_2920723" style="left:471.243px;top:170.315px;"><img id="img11_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr16_2920723" style="left:67.297px;top:283.933px;"><img id="img12_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr17_2920723" style="left:109.54px;top:283.933px;"><img id="img13_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr18_2920723" style="left:188.432px;top:283.933px;"><img id="img14_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr19_2920723" style="left:150.73px;top:283.933px;"><img id="img15_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr20_2920723" style="left:234.162px;top:283.933px;"><img id="img16_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr21_2920723" style="left:276.405px;top:283.933px;"><img id="img17_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr22_2920723" style="left:355.297px;top:283.933px;"><img id="img18_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr23_2920723" style="left:317.594px;top:283.933px;"><img id="img19_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr24_2920723" style="left:398.27px;top:283.933px;"><img id="img20_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr25_2920723" style="left:440.513px;top:283.933px;"><img id="img21_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="spr26_2920723" style="left:660.486px;top:170.315px;"><div style="width:0px;"><span id="txt2_2920723" style="left:7.2px;top:-1.966px;">1</span></div><div style="width:0px;"><span id="txt3_2920723" data-width="19.894531" style="left:29.45px;top:-1.966px;">  </span></div><div style="width:0px;"><span id="txt4_2920723" style="left:49.45px;top:-1.966px;">2</span></div></div><div id="spr27_2920723" style="left:629.676px;top:277.558px;"><div style="width:0px;"><span id="txt5_2920723" class="relpos" style="left:7.2px;top:-1.966px;">+</span><span id="txt6_2920723" class="relpos" style="left:7.195px;top:-1.966px;"> </span></div><div style="width:0px;"><span id="txt7_2920723" style="left:39.075px;top:-1.966px;">1</span></div><div style="width:0px;"><span id="txt8_2920723" data-width="19.894531" style="left:61.325px;top:-1.966px;">  </span></div><div style="width:0px;"><span id="txt9_2920723" style="left:81.325px;top:-1.966px;">1</span></div></div><div id="spr28_2920723" style="left:474.951px;top:283.844px;"><img id="img22_2920723" src="data/img3.png" width="43" height="69" alt="Icon\
+\
+Description automatically generated" style="left:-0.014px;top:-0.25px;"/></div><div id="svg3_2920723" style="left:611.512px;top:341.809px;"><svg width="139" height="6" viewBox="-3 -1.5 139 6"><g fill="#4472c4" stroke="none"><path stroke="#4472c4" stroke-width="3" stroke-linecap="butt" transform="translate(-614.511658,-343.309357)" d="M614.512,344.809 H746.791"/></g></svg></div><div id="spr29_2920723" style="left:629.676px;top:354.509px;"><div style="width:0px;"><span id="txt10_2920723" data-width="29.841797" style="left:7.2px;top:-1.966px;">   </span></div><div style="width:0px;"><span id="txt11_2920723" style="left:37.2px;top:-1.966px;">2</span></div><div style="width:0px;"><span id="txt12_2920723" data-width="19.894531" style="left:59.45px;top:-1.966px;">  </span></div><div style="width:0px;"><span id="txt13_2920723" style="left:79.45px;top:-1.966px;">3</span></div></div><div id="spr30_2920723" style="left:63.598px;top:436.565px;"><div style="width:0px;"><span id="txt14_2920723" data-width="783.609375" style="left:7.2px;top:-0.448px;">What about we group them in 1’s and 10’s and then add the</span></div><div style="width:0px;"><span id="txt15_2920723" data-width="259.953125" style="left:7.2px;top:37.952px;">1’s and add the 10’s</span></div></div><div id="spr31_2920723" style="left:318px;top:500.5px;"><div style="width:0px;"><span id="txt16_2920723" data-width="33.867188" style="left:145.062px;top:5.657px;">C3ML1</span></div></div></div></div>');})();
